93e1b9bc048777e59ef59e40ecfeabc3241373c8
laningya <86898999+laningya@users.noreply.github.com>
2025-04-02 17:35:49 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, MailBulk, Rss, Images, Cloud } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, MailBulk, Rss, Images} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -73,7 +73,6 @@ const siteIcon = {
   MailBulk,
   Rss,
   Images,
-  Cloud,
 };
 
 // 链接跳转
